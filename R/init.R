135851ccec68afce19b91bcdc2c6e7ef098ae5e8
chrisbcl <chrisbcl@hotmail.com>
2014-10-14 14:24:10 +0100

    init.R changed

--- R/init.R
+++ R/init.R
@@ -1,8 +1,8 @@
 names = c("genefilter", "xcms", "impute")
 
-for (n in names){
-	if (!(n %in% rownames(installed.packages()))){
+for (n in 1:length(names)){
+	if (!(names[n] %in% rownames(installed.packages()))){
 		source("http://bioconductor.org/biocLite.R")
-		biocLite(n, character.only = TRUE)
+		biocLite(names[n])
 	}
 }
