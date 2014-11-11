b2e10739d027eb027339ef606f840564799325aa
chrisbcl <chriisbcl@gmail.com>
2014-11-11 22:54:17 +0000

    convert_hyperspec.R edited online with Bitbucket

--- R/convert_hyperspec.R
+++ R/convert_hyperspec.R
@@ -8,7 +8,7 @@ convert.from.hyperspec = function(hsobj, type = "undefined", description = "") {
     metadata = hsobj$..
   else metadata = NULL
   labels = labels(hsobj)
-  print(hsobj)
+  print(labels(hsobj))
   dataset = create.dataset(datamatrix, type = type, metadata = metadata, description = description, 
                             x.axis.values = x.values, label.x = labels$.wavelength, label.values = labels$spc)
   dataset
