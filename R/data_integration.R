25271f212630068b68d95fe6fd93d74510488362
chrisbcl <chrisbcl@hotmail.com>
2014-11-21 14:31:57 +0000

    data integration

--- R/data_integration.R
+++ R/data_integration.R
@@ -21,9 +21,12 @@
 # assuming rownames are not duplicated and first dataset contains all the metadata 
 "fusion.merge" = function(datasets){
 	ds.fused = datasets[[1]]
+	ds.fused$description = paste("Data integration from types: ", ds.fused$type, sep = "")
 	for (i in 2:length(datasets)){
 		ds.fused$data = rbind(ds.fused$data, datasets[[i]]$data)
+		ds.fused$description = paste(ds.fused.description, ds.fused$type, sep = ",")
 	}
 	ds.fused$metadata = datasets[[1]]$metadata
+	ds.fused$type = "integrated-data"
 	ds.fused
 }
