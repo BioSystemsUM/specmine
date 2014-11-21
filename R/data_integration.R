1e42fb021308a483bb7071f38cc9b1060febb52a
chrisbcl <chrisbcl@hotmail.com>
2014-11-21 13:33:16 +0000

    data integration

---
+++ R/data_integration.R
@@ -0,0 +1,28 @@
+########################################################################
+######################## LOW LEVEL FUSION ##############################
+########################################################################
+
+"low.level.fusion" = function(datasets){
+	sample.names = colnames(datasets[[1]]$data)
+	for (i in 2:length(datasets)){
+		sample.names = intersect(sample.names, colnames(datasets[[i]]$data))
+	}
+	subsets = list()
+	for (i in 1:length(datasets)){
+		if (!is.null(datasets[[i]]$metadata)) r.factors = T
+		else r.factors = F
+		subsets[[i]] = subset.samples(datasets[[i]], samples = sample.names, rebuild.factors = r.factors)
+	}
+	
+	ds.fused = fusion.merge(subsets)
+	ds.fused
+}
+
+# assuming rownames are not duplicated
+"fusion.merge" = function(datasets){
+	ds.fused = datasets[[1]]
+	for (i in 2:length(datasets)){
+		ds.fused$data = rbind(ds.fused$data, datasets[[i]]$data)
+	}
+	ds.fused
+}
