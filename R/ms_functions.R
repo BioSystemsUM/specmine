1dabb89577f0b5bc17946c5bf1b65060ae40c249
chrisbcl <chrisbcl@hotmail.com>
2015-01-06 10:50:22 +0000

    ms functions

--- R/ms_functions.R
+++ R/ms_functions.R
@@ -3,7 +3,7 @@
 # use functions in XCMS package
 read.ms.spec<-function(folder.name, profmethod='bin', fwhm=30, bw=30){
   suppressMessages(require(xcms));
-  files <- list.files(folder, recursive=T, full.names=TRUE);
+  files <- list.files(folder.name, recursive=T, full.names=TRUE);
     
   xset <- xcmsSet(msfiles, profmethod = "bin", fwhm=fwhm);
   xset<-group(xset, bw=bw);
