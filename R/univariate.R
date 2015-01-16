7fd06014f82c7f8dc4c89cb6312e406023063d74
chrisbcl <chrisbcl@hotmail.com>
2015-01-16 16:01:08 +0000

    univariate plots

--- R/univariate.R
+++ R/univariate.R
@@ -127,8 +127,9 @@ plot.fold.change = function(dataset, fc.results, fc.threshold, plot.log = T) {
     else cols[i] = "gray"
   if (plot.log) {
     max = max ( max(abs(fc.orig$"log2(FC)")), abs(log2(fc.threshold)) )
-    plot(rownames(fc.orig),fc.orig$"log2(FC)", xlab = get.x.label(dataset), ylab = "Log2FoldChange", 
-         col = cols, pch = 19, ylim = c(-max,max))
+    plot(fc.orig$"log2(FC)", xlab = get.x.label(dataset), ylab = "Log2FoldChange", 
+         col = cols, pch = 19, ylim = c(-max,max), xaxt="n")
+    axis(1, at = 1:length(rownames(fc.orig)),labels = rownames(fc.orig))
     abline(h = log2(fc.threshold), col = "lightblue")
     abline(h = -log2(fc.threshold), col = "lightblue")
     abline(h = 0)
@@ -136,8 +137,9 @@ plot.fold.change = function(dataset, fc.results, fc.threshold, plot.log = T) {
   else {
     max = max ( max(fc.orig$FoldChange), fc.threshold )
     min = min ( min(fc.orig$FoldChange), 1/fc.threshold) 
-    plot(rownames(fc.orig),fc.orig$FoldChange, xlab = get.x.label(dataset), ylab = "FoldChange", 
-         col = cols, pch = 19, ylim = c(min,max))
+    plot(fc.orig$FoldChange, xlab = get.x.label(dataset), ylab = "FoldChange", 
+         col = cols, pch = 19, ylim = c(min,max), xaxt="n")
+    axis(1, at = 1:length(rownames(fc.orig)),labels = rownames(fc.orig))
     abline(h = fc.threshold, col = "lightblue")
     abline(h = 1/fc.threshold, col = "lightblue")
     abline(h = 1)
@@ -188,7 +190,8 @@ plot.ttests = function(dataset, tt.results, tt.threshold = 0.01) {
     if (i %in% tt.lower) cols[i] = "blue"
     else cols[i] = "gray"
   
-  plot(rownames(tt.orig),tt.orig$"-log10", xlab = get.x.label(dataset), ylab = "-log10(p)", col = cols, pch = 19)
+  plot(tt.orig$"-log10", xlab = get.x.label(dataset), ylab = "-log10(p)", col = cols, pch = 19, xaxt="n")
+  axis(1, at = 1:length(rownames(tt.orig)),labels = rownames(tt.orig))
   abline(h = -log10(tt.threshold), col = "lightblue")
 }
 
