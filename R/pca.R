7ec7997b8e5fae01cb70a1683335efb2e7b5469c
chrisbcl <chrisbcl@hotmail.com>
2015-03-06 14:33:28 +0000

    pca biplot

--- R/pca.R
+++ R/pca.R
@@ -158,6 +158,7 @@ pca.biplot= function(dataset, pca.result, cex = 0.8, legend.cex = 0.8, x.colors 
 	stop("Class not supported");
   } 
   if (x.flag){
+	par(mar=c(5.1, 4.1, 4.1, 8.1), xpd=TRUE)
 	legend(legend.place, inset=c(-0.2,0), levels(dataset$metadata[, x.colors]), cex=legend.cex, fill = sort(as.integer(factor(levels(dataset$metadata[, x.colors]))))) 
   }
 }
