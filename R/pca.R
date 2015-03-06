188a9400b3157cd8779b8c7ad289508b0b62ff49
chrisbcl <chrisbcl@hotmail.com>
2015-03-06 14:50:11 +0000

    pca biplot

--- R/pca.R
+++ R/pca.R
@@ -143,7 +143,7 @@ pca.scoresplot3D = function(dataset, pca.result, column.class, pcas=c(1,2,3))
 }
 
 #biplots
-pca.biplot= function(dataset, pca.result, cex = 0.8, legend.cex = 0.8, x.colors = 1, legend.place = "topright", ...) {
+pca.biplot= function(dataset, pca.result, cex = 0.8, legend.cex = 0.8, x.colors = 1, inset = c(-0.4, 0), legend.place = "topright", ...) {
   x.flag = F
   if (x.colors %in% colnames(dataset$metadata)){
 	x.colors.meta = as.integer(dataset$metadata[, x.colors])
@@ -159,7 +159,7 @@ pca.biplot= function(dataset, pca.result, cex = 0.8, legend.cex = 0.8, x.colors 
 	stop("Class not supported");
   } 
   if (x.flag){
-	legend(legend.place, levels(dataset$metadata[, x.colors]), cex=legend.cex, bty="n", fill = sort(as.integer(factor(levels(dataset$metadata[, x.colors]))))) 
+	legend(legend.place, inset = inset, levels(dataset$metadata[, x.colors]), cex=legend.cex, bty="n", fill = sort(as.integer(factor(levels(dataset$metadata[, x.colors]))))) 
 	par(mar=c(5, 4, 4, 2) + 0.1)
   }
 }
