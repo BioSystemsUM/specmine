6aff3a32b7f8858641fe233b7101cada305a0453
chrisbcl <chrisbcl@hotmail.com>
2015-04-07 14:35:18 +0100

    spectral type ms

--- R/structure.R
+++ R/structure.R
@@ -1,8 +1,8 @@
 ## FUNCTIONS TO DEFINE AND QUERY DATASET STRUCTURE
 
-list.of.spectral.types = c("nmr-spectra","ir-spectra", "uvv-spectra", "ms-spectra", "raman-spectra", "fluor-spectra")
+list.of.spectral.types = c("nmr-spectra","ir-spectra", "uvv-spectra", "raman-spectra", "fluor-spectra")
 
-list.of.allowed.types = c(list.of.spectral.types, "nmr-peaks", "lcms-peaks", "gcms-peaks", "concentrations", "integrated-data", "undefined")
+list.of.allowed.types = c(list.of.spectral.types, "ms-spectra", "nmr-peaks", "lcms-peaks", "gcms-peaks", "concentrations", "integrated-data", "undefined")
 
 
 # function to create a dataset from existing objects 
