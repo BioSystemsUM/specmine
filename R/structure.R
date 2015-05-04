78caebc1d077f9b37eef13185200c31a65beb4b7
Chris <chrisbcl@hotmail.com>
2015-05-04 14:47:56 +0100

    metadata row order

--- R/structure.R
+++ R/structure.R
@@ -84,7 +84,7 @@ list.of.allowed.types = c(list.of.spectral.types, "ms-spectra", "nmr-peaks", "lc
           stop("Invalid non numeric values for variable names in rownames of matrix (given spectral type)")
   }
   
-  if (!is.null(metadata){
+  if (!is.null(metadata)){
 	  if (!is.null(rownames(metadata))){
 		metadata.ordered = data.frame(metadata[match(rownames(metadata), colnames(datamatrix)),])
 		colnames(metadata.ordered) = colnames(metadata)
