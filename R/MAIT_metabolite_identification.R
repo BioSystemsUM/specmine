bd676cd551a9ff5cc102644f0de0080672fc46db
Chris <chrisbcl@hotmail.com>
2015-07-12 18:40:44 +0100

    metabolite identification - mait

--- R/MAIT_metabolite_identification.R
+++ R/MAIT_metabolite_identification.R
@@ -4,7 +4,7 @@ MAIT.identify.metabolites = function(dataset, metadata.variable, xSet = NULL, da
 	metadata.var = dataset$metadata[,metadata.variable]
 	mait.object = sampleProcessing.modified(dataDir = data.folder, metadata = metadata.var, xSet = xSet)
 	mait.annotation = peakAnnotation(MAIT.object = mait.object, corrWithSamp = 0.7, corrBetSamp = 0.75, 
-                            perfwhm = 0.6)
+                            perfwhm = 0.6, project = "MAIT")
     mait.sig = spectralSigFeatures(MAIT.object = mait.annotation, pvalue = 0.05, p.adj = "none",
                                scale = FALSE, printCSVfile = F)
     
