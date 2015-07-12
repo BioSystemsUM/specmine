b4553dd682377c481bc0856da28e6e0a87ee484e
Chris <chrisbcl@hotmail.com>
2015-07-12 19:42:40 +0100

    metabolite identification - mait

--- R/MAIT_metabolite_identification.R
+++ R/MAIT_metabolite_identification.R
@@ -57,7 +57,7 @@ sampleProcessing.modified = function (dataDir = NULL, metadata = NULL, xSet = NU
 		classNum[2] <- secondClass 
 		classes <- rep(class, classNum)
 	} else {
-		#class = metadata
+		class = levels(metadata)
 		#classNum <- vector(length = length(table(class)))
 		#for (i in 1:length(classNum)){
 	#		classNum[i] = as.numeric(table(class)[i])
