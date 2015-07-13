38b64cb65a79df5746cfbf271c1139bdf21e0ce4
Chris <chrisbcl@hotmail.com>
2015-07-13 20:15:49 +0100

    ms functions updated

--- R/ms_functions.R
+++ R/ms_functions.R
@@ -1,11 +1,11 @@
 # A commonly used value is 30 (seconds) for LC-MS and 4 (seconds) for GC-MS spectra (fwhm).
 # read LC/GC-MS spectra(.netCDF, .mzXML, mzData)
 # use functions in XCMS package
-read.ms.spec<-function(folder.name, profmethod='bin', fwhm=30, bw=30){
+read.ms.spec<-function(folder.name, metadata = NULL, profmethod='bin', fwhm=30, bw=30){
   suppressMessages(require(xcms));
   files <- list.files(folder.name, recursive=T, full.names=TRUE);
-    
-  xset <- xcmsSet(files, profmethod = "bin", fwhm=fwhm);
+  if (is.null(metadata) xset <- xcmsSet(files, profmethod = "bin", fwhm=fwhm);
+  else xset = xcmsSet(files, profmethod = "bin", fwhm = fwhm, sclass = metadata[,1);
   xset<-group(xset, bw=bw);
   xset
 }
@@ -44,7 +44,7 @@ read.ms.spectra = function(folder.name, type = "undefined", filename.meta= NULL,
 	if (!is.null(filename.meta))
 		metadata = read.metadata(filename.meta, header.col = header.col.meta, header.row = header.row.meta, sep = sep.meta)
 	else metadata = NULL
-	xset = read.ms.spec(folder.name, prof.method, fwhm, bw)
+	xset = read.ms.spec(folder.name, metadata = metadata, prof.method, fwhm, bw)
 	xset = ms.rt.correction(xset, bw)
 	xset = ms.fill.peaks(xset)
 	mat = ms.create.matrix(xset, intvalue)
