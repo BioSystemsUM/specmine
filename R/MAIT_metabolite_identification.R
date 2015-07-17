838f20158198da13f102ead937c750ce3d86130a
Chris <chrisbcl@hotmail.com>
2015-07-17 20:48:31 +0100

    mait metabolite

--- R/MAIT_metabolite_identification.R
+++ R/MAIT_metabolite_identification.R
@@ -1,6 +1,9 @@
 MAIT.identify.metabolites = function(dataset, metadata.variable, xSet = NULL, data.folder = NULL, features = NULL, 
 									 mass.tolerance = 0.5){
-	
+	imports = parent.env(getNamespace("CAMERA"))
+	unlockBinding("groups", imports)
+	imports[["groups"]] = xcms::groups
+	lockBinding("groups", imports)
 	metadata.var = dataset$metadata[,metadata.variable]
 	mait.object = sampleProcessing.modified(dataDir = data.folder, metadata = metadata.var, xSet = xSet, project = "MAIT")
 	mait.annotation = peakAnnotation(MAIT.object = mait.object, corrWithSamp = 0.7, corrBetSamp = 0.75, 
@@ -58,10 +61,10 @@ sampleProcessing.modified = function (dataDir = NULL, metadata = NULL, xSet = NU
 		classes <- rep(class, classNum)
 	} else {
 		class = levels(metadata)
-		#classNum <- vector(length = length(table(class)))
-		#for (i in 1:length(classNum)){
-	#		classNum[i] = as.numeric(table(class)[i])
-		#}
+		classNum <- vector(length = length(table(class)))
+		for (i in 1:length(classNum)){
+			classNum[i] = as.numeric(table(class)[i])
+		}
 		classes <- as.character(metadata)
 	}
 	
