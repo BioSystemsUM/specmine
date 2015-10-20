9a4485f2e6f2519e8678d339e02852af852ea6e6
chrisbcl <chrisbcl@hotmail.com>
2015-10-20 10:18:03 +0100

    mait unsafe fixed(?)

--- R/MAIT_metabolite_identification.R
+++ R/MAIT_metabolite_identification.R
@@ -1,9 +1,6 @@
 MAIT_identify_metabolites = function(dataset, metadata.variable, xSet = NULL, data.folder = NULL, features = NULL, 
 									 mass.tolerance = 0.5){
-	imports = parent.env(getNamespace("CAMERA"))
-	unlockBinding("groups", imports)
-	imports[["groups"]] = xcms::groups
-	lockBinding("groups", imports)
+
 	metadata.var = dataset$metadata[,metadata.variable]
 	mait.object = sampleProcessing_modified(dataDir = data.folder, metadata = metadata.var, xSet = xSet, project = "MAIT")
 	mait.annotation = MAIT::peakAnnotation(MAIT.object = mait.object, corrWithSamp = 0.7, corrBetSamp = 0.75, 
