b59981aae05e0d0da003fa4c996089a2d3915de0
TelmaA <telma.afonso94@gmail.com>
2017-05-24 14:31:38 +0100

    Fixed read_dataset_dx and read_data_dx functions. JDX files are now read using the readJDX package.

--- R/read_dx.R
+++ R/read_dx.R
@@ -10,10 +10,10 @@
   
   data.dx = read_data_dx(folder.data)
   
-  freqs = data.dx[[1]][,1] # get frequencies from first spectrum
+  freqs = data.dx[[1]][[2]][,1] # get frequencies from first spectrum
   datamat = matrix(data = NA, nrow = length(freqs), ncol = length(data.dx))
 
-  for (i in 1:length(data.dx)) datamat[,i] = data.dx[[i]][,2]
+  for (i in 1:length(data.dx)) datamat[,i] = data.dx[[i]][[2]][,2]
   
   rownames(datamat) = as.character(freqs)
   colnames(datamat) = names(data.dx)
@@ -23,7 +23,7 @@
   dataset
 }
 
-"read_data_dx" = function(foldername, debug = F)
+"read_data_dx" = function(foldername, debug = 0)
 {
   filenames = dir(foldername, pattern=".[Dd][Xx]$", full.names=TRUE)
   sampleList = list()
@@ -31,7 +31,7 @@
   snames <- gsub("\\.[^.]*$", "", basename(filenames));
   for (i in 1:length(filenames)) {
     print(paste("Reading sample ", filenames[i]))
-    sampleList[[i]] = readJDX_specmine(filenames[i], debug = debug)
+    sampleList[[i]] = readJDX::readJDX(filenames[i], debug = debug, SOFC = F) #If TRUE it fails for files that don't have 'firstY' metadata
   }
   sampleNames = snames
   names(sampleList) = sampleNames
@@ -44,78 +44,78 @@
   samples.names
 }
 
-"readJDX_specmine" = function (file = "", debug = FALSE) 
-{
-  if (!requireNamespace("gsubfn", quietly = TRUE)) {
-    stop("You need to install package gsubfn to use this option")
-  }
-  if (file == "") stop("No file specified")
-  jdx <- readLines(file)
-  cmpd <- grep("^##TITLE=.*$", jdx)
-  if (debug) cat("CMPD number:" , cmpd)
-  if (cmpd > 1) stop("Compound data sets not supported")
-  if (cmpd == 0) stop("This may not be a JCAMP-DX file")
-  ntup <- grepl("^##NTUPLES", jdx)
-  if (any(ntup)) 
-    stop("This looks like NMR data with real & imaginary parts, which is not supported")
-  if (debug) cat("\nFile = ", file, "\n")
-  spcstart <- grep("^##XYDATA=\\s*\\(X\\+\\+\\(Y\\.\\.Y\\)\\)$",  jdx) 
-  if (debug) cat ("SPCSTART" , spcstart, "\n")
-  if (spcstart == 0) stop("Couldn't find the data block start (see ?readJDX for supported formats)")
-  spcstart = spcstart + 1
-  
-  if (debug) print(jdx[1:(spcstart - 2)])
-  
-  spcend <- grep("^##END=", jdx) - 1
-  if (debug) cat ("SPCEND" , spcend, "\n")
-  if (spcend == 0) stop("Couldn't find the data block end")
-
-  if (!length(spcstart) == 1L | !length(spcend) == 1L) 
-    stop("Problem with delimiting data block")
-  if (!spcstart < spcend) 
-    stop("End of data block in the wrong place")
-
-  yValues <- jdx[spcstart:spcend]
-  for (n in 1:length(yValues)) {
-    yValues[n] <- sub("\\s*(\\+|-)*[[:digit:]]+(\\.|,)?[[:digit:]]*\\s*", "", yValues[n])
-  }
-  yValues <- paste(yValues, collapse = " ")
-  yValues <- gsub("\\+", " ", yValues)
-  yValues <- gsub("-", " -", yValues)
-  yValues <- sub("\\s*", "", yValues)
-  yValues <- gsub(",", ".", yValues)
-  yValues <- strsplit(yValues, split = "\\s+")
-  yValues <- as.numeric(unlist(yValues))
-  firstX <- grep("^##FIRSTX=", jdx)
-  if (firstX == 0)stop("Couldn't find FIRSTX")
-  firstX <- jdx[firstX]
-  firstX <- gsubfn::gsubfn("##FIRSTX=", replacement = "", firstX)
-  firstX <- sub(",", ".", firstX)
-  firstX <- as.numeric(firstX)
-  lastX <- grep("^##LASTX=", jdx)
-  if (lastX == 0) stop("Couldn't find LASTX")
-  lastX <- jdx[lastX]
-  lastX <- gsubfn::gsubfn("##LASTX=", replacement = "", lastX)
-  lastX <- sub(",", ".", lastX)
-  lastX <- as.numeric(lastX)
-  npoints <- grep("^##NPOINTS=", jdx)
-  if (npoints == 0) stop("Couldn't find NPOINTS")
-  npoints <- jdx[npoints]
-  npoints <- gsubfn::gsubfn("##NPOINTS=", replacement = "", npoints)
-  npoints <- as.integer(npoints)
-  if (debug) cat("NPOINTS = ", npoints, "\n")
-  if (debug)  cat("Actual no. data points found  = ", length(yValues), "\n")
-  if (!npoints == length(yValues)) stop("NPOINTS and length of data block don't match")
-  if (debug) cat("firstX = ", firstX, "\n")
-  if (debug) cat("lastX = ", lastX, "\n")
-  yFac <- grep("^##YFACTOR=", jdx)
-  if (yFac == 0) stop("Couldn't find YFACTOR")
-  yFac <- gsubfn::gsubfn("##YFACTOR=", replacement = "", jdx[yFac])
-  yFac <- sub(",", ".", yFac)
-  yFac <- as.numeric(yFac)
-  if (debug) cat("yFac = ", yFac, "\n")
-  yValues <- yValues * yFac
-  actDX <- (lastX - firstX)/(npoints - 1)
-  xValues <- seq(firstX, lastX, by = actDX)
-  res <- data.frame(x = xValues, y = yValues)
-}
+# "readJDX_specmine" = function (file = "", debug = FALSE) 
+# {
+#   if (!requireNamespace("gsubfn", quietly = TRUE)) {
+#     stop("You need to install package gsubfn to use this option")
+#   }
+#   if (file == "") stop("No file specified")
+#   jdx <- readLines(file)
+#   cmpd <- grep("^##TITLE=.*$", jdx)
+#   if (debug) cat("CMPD number:" , cmpd)
+#   if (cmpd > 1) stop("Compound data sets not supported")
+#   if (cmpd == 0) stop("This may not be a JCAMP-DX file")
+#   ntup <- grepl("^##NTUPLES", jdx)
+#   if (any(ntup)) 
+#     stop("This looks like NMR data with real & imaginary parts, which is not supported")
+#   if (debug) cat("\nFile = ", file, "\n")
+#   spcstart <- grep("^##XYDATA=\\s*\\(X\\+\\+\\(Y\\.\\.Y\\)\\)$",  jdx) 
+#   if (debug) cat ("SPCSTART" , spcstart, "\n")
+#   if (spcstart == 0) stop("Couldn't find the data block start (see ?readJDX for supported formats)")
+#   spcstart = spcstart + 1
+#   
+#   if (debug) print(jdx[1:(spcstart - 2)])
+#   
+#   spcend <- grep("^##END=", jdx) - 1
+#   if (debug) cat ("SPCEND" , spcend, "\n")
+#   if (spcend == 0) stop("Couldn't find the data block end")
+# 
+#   if (!length(spcstart) == 1L | !length(spcend) == 1L) 
+#     stop("Problem with delimiting data block")
+#   if (!spcstart < spcend) 
+#     stop("End of data block in the wrong place")
+# 
+#   yValues <- jdx[spcstart:spcend]
+#   for (n in 1:length(yValues)) {
+#     yValues[n] <- sub("\\s*(\\+|-)*[[:digit:]]+(\\.|,)?[[:digit:]]*\\s*", "", yValues[n])
+#   }
+#   yValues <- paste(yValues, collapse = " ")
+#   yValues <- gsub("\\+", " ", yValues)
+#   yValues <- gsub("-", " -", yValues)
+#   yValues <- sub("\\s*", "", yValues)
+#   yValues <- gsub(",", ".", yValues)
+#   yValues <- strsplit(yValues, split = "\\s+")
+#   yValues <- as.numeric(unlist(yValues))
+#   firstX <- grep("^##FIRSTX=", jdx)
+#   if (firstX == 0)stop("Couldn't find FIRSTX")
+#   firstX <- jdx[firstX]
+#   firstX <- gsubfn::gsubfn("##FIRSTX=", replacement = "", firstX)
+#   firstX <- sub(",", ".", firstX)
+#   firstX <- as.numeric(firstX)
+#   lastX <- grep("^##LASTX=", jdx)
+#   if (lastX == 0) stop("Couldn't find LASTX")
+#   lastX <- jdx[lastX]
+#   lastX <- gsubfn::gsubfn("##LASTX=", replacement = "", lastX)
+#   lastX <- sub(",", ".", lastX)
+#   lastX <- as.numeric(lastX)
+#   npoints <- grep("^##NPOINTS=", jdx)
+#   if (npoints == 0) stop("Couldn't find NPOINTS")
+#   npoints <- jdx[npoints]
+#   npoints <- gsubfn::gsubfn("##NPOINTS=", replacement = "", npoints)
+#   npoints <- as.integer(npoints)
+#   if (debug) cat("NPOINTS = ", npoints, "\n")
+#   if (debug)  cat("Actual no. data points found  = ", length(yValues), "\n")
+#   if (!npoints == length(yValues)) stop("NPOINTS and length of data block don't match")
+#   if (debug) cat("firstX = ", firstX, "\n")
+#   if (debug) cat("lastX = ", lastX, "\n")
+#   yFac <- grep("^##YFACTOR=", jdx)
+#   if (yFac == 0) stop("Couldn't find YFACTOR")
+#   yFac <- gsubfn::gsubfn("##YFACTOR=", replacement = "", jdx[yFac])
+#   yFac <- sub(",", ".", yFac)
+#   yFac <- as.numeric(yFac)
+#   if (debug) cat("yFac = ", yFac, "\n")
+#   yValues <- yValues * yFac
+#   actDX <- (lastX - firstX)/(npoints - 1)
+#   xValues <- seq(firstX, lastX, by = actDX)
+#   res <- data.frame(x = xValues, y = yValues)
+# }
